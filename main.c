86de9ae85ef1f7ed0f28b7797a33ac35ecf5fe37
Godwin_jnr <Chibuezegodwin49@gmail.com>
2026-08-28 05:46:39 +0100

    betty check

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+/**
+ * add - Add two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
+ * Return: The result of added integers
+ */
+
+	int add(int num1, int num2)
+	{
+		return (num1 + num2);
+	}
+
+	int main(){
+	
+	return 0;
+	}
