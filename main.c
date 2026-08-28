fdefd9a8e26428fd7149676c7813683a22560116
Sir2fa <Chibuezegodwin49@gmail.com>
2026-08-28 12:24:09 +0100

    betty check

--- main.c
+++ main.c
@@ -3,9 +3,8 @@
  * add - Add two integers.
  * @num1: The first integer to be added.
  * @num2: The second integer to be added.
- *
  * main - Header function.
- * @void: empty.
+ *
  * Return: The result of added integers
  */
 
@@ -14,7 +13,7 @@
 		return (num1 + num2);
 	}
 
-	int main(void) 
+	int main(void)
 	{
 	return (0);
 	}
